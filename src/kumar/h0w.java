6e319b5531d195a4170d7a7c13dd1b66c09d46e7
User <User@Kumar>
2019-10-11 09:48:07 +0530

    Added home page..

---
+++ src/kumar/h0w.java
@@ -0,0 +1,20 @@
+package kumar;
+
+public class h0w {
+	public static void main(String[] args) {
+		int tamil=40;
+		int english=50;
+		int maths=20;
+        int science=24;
+        int social=25;
+        int D=(tamil+english+maths+science+social);
+        int E=(D/5);
+        if(E<=25) {
+        System.out.println("Fail");
+        }
+        if(E<=50&&E<=40) {
+        System.out.println("pass");
+	}
+
+}
+}
